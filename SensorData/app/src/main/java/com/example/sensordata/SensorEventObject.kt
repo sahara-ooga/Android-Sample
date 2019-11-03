4d79f59b58857a494ebe926aaa4dd0ff4e2bb6d2
yuu ogasawara <performa532@gmail.com>
2019-11-03 11:52:36 +0900

    Add Accuracy Object to SensorEventObject

--- SensorData/app/src/main/java/com/example/sensordata/SensorEventObject.kt
+++ SensorData/app/src/main/java/com/example/sensordata/SensorEventObject.kt
@@ -1,11 +1,23 @@
 package com.example.sensordata
 
 import android.hardware.SensorEvent
+import android.hardware.SensorManager.*
 
-class SensorEventObject(
-    val accuracy: Int,
-    val timestamp: Long,
-    val values: FloatArray
-) {
-    constructor(event: SensorEvent) : this(event.accuracy, event.timestamp, event.values)
+class SensorEventObject(event: SensorEvent) {
+    val accuracy: Accuracy = when (event.accuracy) {
+        SENSOR_STATUS_ACCURACY_LOW -> Accuracy.LOW
+        SENSOR_STATUS_ACCURACY_MEDIUM -> Accuracy.MEDIUM
+        SENSOR_STATUS_ACCURACY_HIGH -> Accuracy.HIGH
+        else -> Accuracy.UNKNOWN
+    }
+    val timestamp: Long = event.timestamp
+    val values: FloatArray = event.values
+
+    /*
+    * Sensor Accuracy Object
+    * see also https://developer.android.com/reference/android/hardware/SensorManager.html
+    * */
+    enum class Accuracy {
+        LOW, MEDIUM, HIGH, UNKNOWN
+    }
 }
