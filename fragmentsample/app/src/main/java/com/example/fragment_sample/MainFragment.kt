19939dfefe4f58a8be4d699a89c06ba62dc97df1
yuu ogasawara <performa532@gmail.com>
2019-11-26 21:05:34 +0900

    display fragment

---
+++ fragmentsample/app/src/main/java/com/example/fragment_sample/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragment_sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
