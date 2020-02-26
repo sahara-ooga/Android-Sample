28b9acf1c6a0e2eb4d0dbf148d0bcc2bb79aa4f7
yuu ogasawara <performa532@gmail.com>
2020-02-26 15:16:40 +0900

    add observe power sample

---
+++ AndroidObservePowerSample/app/src/main/java/example/android/android_observe_power_sample/MainActivity.kt
@@ -0,0 +1,22 @@
+package example.android.android_observe_power_sample
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        registerBroadcastReceivers()
+    }
+    private fun registerBroadcastReceivers() {
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+            addAction(Intent.ACTION_POWER_CONNECTED)
+        }
+        registerReceiver(MyBroadcastReceiver(), intentFilter)
+    }
+}
---
+++ AndroidObservePowerSample/app/src/main/java/example/android/android_observe_power_sample/MyBroadcastReceiver.kt
@@ -0,0 +1,44 @@
+package example.android.android_observe_power_sample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    private val OPEN_APP_THRESHOLD: Long = 5
+    var lastOpenDate = System.currentTimeMillis()
+
+    /**
+     * 電源供給の変化を検出したら、画面を開く
+     *
+     * @param context The Context in which the receiver is running.
+     * @param intent The Intent being received.
+     */
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.let {
+            //Check the Intent action and perform the required operation
+            if (it.action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+                Log.d("MyBroadcastReceiver", "ACTION_POWER_DISCONNECTED")
+                startActivity("ACTION_POWER_DISCONNECTED", context)
+            } else if (it.action.equals(Intent.ACTION_POWER_CONNECTED)) {
+                Log.d("MyBroadcastReceiver", "ACTION_POWER_CONNECTED")
+                startActivity("ACTION_POWER_CONNECTED", context)
+            }
+        }
+    }
+
+    private fun startActivity(message: String, context: Context?) {
+        val now = System.currentTimeMillis()
+        val difsec = (now - lastOpenDate) / 1000L
+        if (difsec < OPEN_APP_THRESHOLD) {
+            return
+        }
+
+        val startIntent = Intent(context, MainActivity::class.java).apply {
+            putExtra("example.android.android_observe_power_sample", message)
+        }
+        context?.startActivity(startIntent)
+    }
+}
